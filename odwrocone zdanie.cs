cde2cad2456d0ae9d243f155173bf0c2d97405f0
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2020-05-25 13:47:59 +0200

    Add files via upload

---
+++ odwrocone zdanie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace odwroceniezdania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wprowadz zdanie: ");
+            string[] tablica = Console.ReadLine().Split(' ');
+            int dlugosc = tablica.Length;
+            {
+                Console.WriteLine("Odwrocone zdanie: ");
+            }
+            for (int i = dlugosc - 1; i >= 0; i--)
+            {
+                Console.Write(tablica[i] + " ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
