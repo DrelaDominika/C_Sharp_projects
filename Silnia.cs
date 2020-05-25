efc28058332ce9325b4341b5ea802062f78fd224
Dominika Drela <61236771+DrelaDominika@users.noreply.github.com>
2020-05-25 13:46:10 +0200

    Add files via upload

---
+++ Silnia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Silnia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz cyfrę aby policzyć silnie");
+            int liczba = int.Parse(Console.ReadLine());
+
+            int silnia = 1;
+            for (int i = 1; i <= liczba; i++)
+            {
+                silnia *= i;
+            }
+
+            Console.WriteLine("Silnia dla liczby" + liczba + "to" + silnia);
+
+            Console.ReadLine();
+
+        }
+    }
+}
